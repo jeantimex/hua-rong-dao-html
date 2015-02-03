4dae0f2e32803087e97aa8230382748e0b2f8a00
Yong Su <jean.timex@gmail.com>
2015-02-02 23:25:36 -0800

    Added contact page.

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name klotskiApp.controller:MainCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the klotskiApp
+ */
+angular.module('gameApp')
+  .controller('ContactCtrl', function ($scope) {
+    
+  });
---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name klotskiApp.controller:MainCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the klotskiApp
+ */
+angular.module('gameApp')
+  .controller('HomeCtrl', function ($scope) {
+    
+  });
