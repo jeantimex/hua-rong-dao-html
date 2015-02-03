b2bfca9c099ea5f3654fe08bbb8fb20f51dd392a
Yong Su <jean.timex@gmail.com>
2015-02-03 00:04:49 -0800

    Updated indent.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,38 +9,38 @@
  * Main module of the application.
  */
 angular
-  .module('gameApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'gameManager'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/game', {
-        templateUrl: 'views/game.html'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-  .controller('AppCtrl', function ($scope, $location) {
-    $scope.isActive = function (loc) { 
-      return loc === $location.path();
-    };
-  });
+.module('gameApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'gameManager'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/game', {
+      templateUrl: 'views/game.html'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .when('/contact', {
+      templateUrl: 'views/contact.html',
+      controller: 'ContactCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+})
+.controller('AppCtrl', function ($scope, $location) {
+  $scope.isActive = function (loc) { 
+    return loc === $location.path();
+  };
+});
