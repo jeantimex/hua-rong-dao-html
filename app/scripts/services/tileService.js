5e92c249ae3cf131814c99159e515eda9368b865
Yong Su <jean.timex@gmail.com>
2015-02-11 01:24:10 -0800

    Implemented the basic move algorithm for tile type 1.

--- app/scripts/services/tileService.js
+++ app/scripts/services/tileService.js
@@ -8,6 +8,10 @@ angular.module('GameApp')
 .service('TileService', function () {
 
   this.grid = [];
+  this.path = [];
+  this.stack = [];
+  this.visited = {};
+  this.lastTile = null;
 
   /**
    * Initialize the grid
@@ -16,7 +20,11 @@ angular.module('GameApp')
     var i, j = 0;
 
     this.grid = [];
-    
+
+    this.lastTile = null;
+
+    this.reset();
+
     for (i = 0; i < 5; i++) {
       this.grid[i] = [];
       for (j = 0; j < 4; j++) {
@@ -50,6 +58,13 @@ angular.module('GameApp')
     }
   };
 
+  this.reset = function () {
+    this.path = [];
+    this.stack = [];
+    this.visited = {};
+
+  };
+
   this.printGrid = function () {
     for (var i = 0; i < 5; i++) {
       console.log(this.grid[i]);
@@ -60,7 +75,123 @@ angular.module('GameApp')
    * Move tile
    */
   this.moveTile = function (tile) {
-    
+    if (tile !== this.lastTile) {
+      this.reset();
+    }
+    this.lastTile = tile;
+
+    var i;
+
+    // step 1. Check all directions and find out the possible positions
+    var positions = this.getUnvisitedPositions(tile);
+    //console.log('----------------------');
+    //console.log('positions: ' + positions);
+
+    if (!this.visited.hasOwnProperty(tile.pos)) {
+      for (i = 0; i < positions.length; i++) {
+        if (this.stack.indexOf(positions[i]) === -1) {
+          this.stack.push(positions[i]);
+        }
+      }
+
+      // mark current pos as visited
+      this.visited[tile.pos] = true;
+    }
+
+    //console.log('visited: ' + this.visited);
+    //console.log('stack: ' + this.stack);
+
+    if (this.stack.length > 0) {
+      var index = -1;
+
+      for (i = positions.length - 1; i >= 0; i--) {
+        index = this.stack.indexOf(positions[i]);
+        if (index >= 0) {
+          break;
+        }
+      }
+
+      this.cleanTile(tile);
+
+      if (index >= 0) {
+        this.path.push(tile.pos);
+        tile.pos = this.stack[index];
+        this.stack.splice(index, 1);
+      } else {
+        tile.pos = this.path.pop();
+      }
+
+      this.markTile(tile);
+    }
+
+    //console.log('path: ' + this.path);
+    //console.log('stack: ' + this.stack);
+
+    positions = this.getUnvisitedPositions(tile);
+    //console.log('positions: ' + positions);
+
+    if (positions.length === 0 && this.stack.length === 0) {
+      this.reset();
+    }
+  };
+
+  this.calPos = function (row, col) {
+    return row * 4 + col;
+  };
+
+  /**
+   * Get movable positions in up, down, left and right directions
+   * @param tile
+   * @returns {Array}
+   */
+  this.getUnvisitedPositions = function (tile) {
+    var res = [];
+
+    var pos = tile.pos,
+        row = parseInt(pos / 4),
+        col = parseInt(pos % 4);
+
+    // up
+    if (row > 0 && this.grid[row - 1][col] === 0) {
+      res.push(this.calPos(row - 1, col));
+    }
+
+    // right
+    if (col < 3 && this.grid[row][col + 1] === 0) {
+      res.push(this.calPos(row, col + 1));
+    }
+
+    // left
+    if (col > 0 && this.grid[row][col - 1] === 0) {
+      res.push(this.calPos(row, col - 1));
+    }
+
+    // down
+    if (row < 4 && this.grid[row + 1][col] === 0) {
+      res.push(this.calPos(row + 1, col));
+    }
+
+    for (var i = res.length - 1; i >= 0; i--) {
+      if (this.visited.hasOwnProperty(res[i])) {
+        res.splice(i, 1);
+      }
+    }
+
+    return res;
+  };
+
+  this.cleanTile = function (tile) {
+    var row = parseInt(tile.pos / 4),
+        col = parseInt(tile.pos % 4);
+
+    this.grid[row][col] = 0;
+  };
+
+  this.markTile = function (tile) {
+    var row = parseInt(tile.pos / 4),
+        col = parseInt(tile.pos % 4);
+
+    this.grid[row][col] = tile.type;
   };
 
 });
