7530f9faf5874a5ad8ac372468bb126c8403cab1
Yong Su <jean.timex@gmail.com>
2015-02-13 23:10:44 -0800

    Implemented puzzle solve algorithm.

--- app/scripts/services/levelService.js
+++ app/scripts/services/levelService.js
@@ -15,8 +15,11 @@ angular.module('GameApp')
   this.getLevelData = function () {
     var deferred = $q.defer();
 
+    var self = this;
+
     $http.get(dataUrl)
       .success(function (data) {
+        self.processData(data);
         deferred.resolve(data);
       })
       .error(function () {
@@ -24,6 +27,23 @@ angular.module('GameApp')
       });
 
     return deferred.promise;
+  };
+
+  /**
+   *
+   */
+  this.processData = function (data) {
+    if (angular.isArray(data)) {
+      for (var i = 0; i < data.length; i++) {
+        var tiles = data[i].tiles;
+
+        if (angular.isArray(tiles)) {
+          for (var j = 0; j < tiles.length; j++) {
+            tiles[j].id = j;
+          }
+        }
+      }
+    }
   };
 
 });
