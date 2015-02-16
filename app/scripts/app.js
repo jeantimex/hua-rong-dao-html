9115e477c3a3408f0d8cb183e7c19d5342c16347
Yong Su <jean.timex@gmail.com>
2015-02-15 20:43:43 -0800

    Removed contact page.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,10 +24,6 @@ angular
       templateUrl: 'views/about.html',
       controller: 'AboutCtrl'
     })
-    .when('/contact', {
-      templateUrl: 'views/contact.html',
-      controller: 'ContactCtrl'
-    })
     .otherwise({
       redirectTo: '/'
     });
